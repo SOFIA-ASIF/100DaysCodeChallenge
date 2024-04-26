76159f4728cb3cf1222468c4e572ebf5cc0cc4fd
SOFIA-ASIF <160203172+SOFIA-ASIF@users.noreply.github.com>
2024-04-26 00:17:07 -0700

    adding 4 days work

---
+++ alert.js
@@ -0,0 +1,10 @@
+alert("I am a second file JavaScript")
+let name1;
+let admin;
+name1 = "john";
+admin = name1;
+alert(admin)
+let ourPlanet = "Earth";
+let currentVisitor;
+const BIRTHDAY = '18 june 2005';
+const age = someCode(BIRTHDAY);
